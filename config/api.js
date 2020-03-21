8dcbb3c9673948384f889d37345dfc935c3fa757
quan1993 <jipengquan2015@163.com>
2020-03-21 17:34:50 +0800

    remove gou

--- config/api.js
+++ config/api.js
@@ -18,14 +18,6 @@ module.exports = {
   BrandList: ApiRootUrl + 'brand/list',  //品牌列表
   BrandDetail: ApiRootUrl + 'brand/detail',  //品牌详情
 
-  CartList: ApiRootUrl + 'cart/index', //获取购物车的数据
-  CartAdd: ApiRootUrl + 'cart/add', // 添加商品到购物车
-  CartUpdate: ApiRootUrl + 'cart/update', // 更新购物车的商品
-  CartDelete: ApiRootUrl + 'cart/delete', // 删除购物车的商品
-  CartChecked: ApiRootUrl + 'cart/checked', // 选择或取消选择商品
-  CartGoodsCount: ApiRootUrl + 'cart/goodscount', // 获取购物车商品件数
-  CartCheckout: ApiRootUrl + 'cart/checkout', // 下单前信息确认
-
   OrderSubmit: ApiRootUrl + 'order/submit', // 提交订单
   PayPrepayId: ApiRootUrl + 'pay/prepay', //获取微信统一下单prepay_id
 
@@ -45,18 +37,6 @@ module.exports = {
   SearchHelper: ApiRootUrl + 'search/helper',  //搜索帮助
   SearchClearHistory: ApiRootUrl + 'search/clearhistory',  //搜索帮助
 
-  AddressList: ApiRootUrl + 'address/list',  //收货地址列表
-  AddressDetail: ApiRootUrl + 'address/detail',  //收货地址详情
-  AddressSave: ApiRootUrl + 'address/save',  //保存收货地址
-  AddressDelete: ApiRootUrl + 'address/delete',  //保存收货地址
-
   RegionList: ApiRootUrl + 'region/list',  //获取区域列表
 
-  OrderList: ApiRootUrl + 'order/list',  //订单列表
-  OrderDetail: ApiRootUrl + 'order/detail',  //订单详情
-  OrderCancel: ApiRootUrl + 'order/cancel',  //取消订单
-  OrderExpress: ApiRootUrl + 'order/express', //物流详情
-
-  FootprintList: ApiRootUrl + 'footprint/list',  //足迹列表
-  FootprintDelete: ApiRootUrl + 'footprint/delete',  //删除足迹
 };
